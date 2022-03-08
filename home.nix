5d75b159c5afdcc3ac49844d094d3b4abc744a11
David Francoeur <david.francoeur@disneystreaming.com>
2022-03-08 13:03:50 -0500

    Git config and more programs

--- home.nix
+++ home.nix
@@ -1,8 +1,23 @@
 { pkgs, ... }: {
     home.packages = [ 
-        pkgs.htop
-        pkgs.hello
-        pkgs.shellcheck
+        pkgs.bat
         pkgs.direnv
+        pkgs.fzf
+        pkgs.gnupg
+        pkgs.hello
+        pkgs.htop
+        pkgs.jq
+        pkgs.shellcheck
     ];
+
+    imports = [
+        ./programs/git.nix
+    ];
+
+    home.sessionVariables = {
+      LANG = "en_US.UTF-8";
+      EDITOR = "vim";
+      # Apparently nix-direnv or direnv sets this to something weird
+      GNUPGHOME = "~/.gnupg";
+    };
 }
---
+++ programs/git.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }: {
+  programs.git = {
+    enable = true;
+    userName = "David Francoeur";
+    userEmail = "dfrancoeur04@gmail.com";
+
+    delta.enable = true;
+
+    aliases = {
+      co = "checkout";
+      cob = "checkout -b";
+      pu = "push -u";
+      rsh = "reset --hard HEAD";
+    };
+
+    ignores = [
+      ".metals/"
+      ".history/"
+      "**/project/metals.sbt"
+      ".idea/"
+      ".vscode/settings.json"
+      ".bloop/"
+      ".bsp/"
+    ];
+
+    includes = [
+      {
+        condition = "gitdir:~/workspace/work";
+        contents.user = {
+          name = "David Francoeur";
+          email = "david.francoeur@disneystreaming.com";
+        };
+      }
+    ];
+
+    extraConfig = {
+      pull = { ff = "only"; };
+      init.defaultBranch = "main";
+    };
+  };
+}
